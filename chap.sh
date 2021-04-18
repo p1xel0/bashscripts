41b5e85e7e9ec0facdfcccdda1881a714dca6a3d
p1xel0 <p1xel@NanoS.localdomain>
2021-04-17 20:20:45 -0400

    undelete comments

--- chap.sh
+++ chap.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+##
+# Extremly buggy and unclean code for splitting singular chapter files from BDMV sources
+# Video input is not configurable script expects sources to be mp4 encodes of m2ts from BDMV i.e. 00001.mp4
+# Source chapter file in XML format is required
+# This script works by using ffprobe to get the length of each video and accumulating this total in a loop
+# and splitting based on the closest timestamp to the total runtime while still being less than the total runtime
+# This script expects that your source has a chapter realtively close to the end of each video
+# Usage: ./path_of_script /path/to/chapters.xml
+##
 if [ -z "$1" ]; then
 	echo "error: no xml input" && exit 1
 elif [ -d "$1" ]; then
